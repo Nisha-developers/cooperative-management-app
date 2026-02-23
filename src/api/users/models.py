160a7c0c214f0e8f01b6adfb09065bc7b3f55738
Dominion <adebiyiquawiy2006@gmail.com>
2026-02-23 22:26:04 +0100

    added user model

--- src/api/users/models.py
+++ src/api/users/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
+import random
+from django.utils import timezone
+from datetime import timedelta
+from api.v1.school.models import School, Department
 
 # Create your models here.
+
+class UserGender(models.TextChoices):
+    MALE = "male", "Male"
+    FEMALE = "female", "Female"
+    OTHER = "other", "Other"
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100)
+    
+    gender = models.CharField(
+        max_length=20, choices=UserGender.choices, null=True, blank=True
+    )
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'role'] 
+    verification_code = models.CharField(max_length=6, blank=True, null=True)
+    code_expiry = models.DateTimeField(blank=True, null=True)
+    email_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+
+    def generate_verification_code(self):
+        code = f"{random.randint(100000, 999999)}"
+        self.verification_code = code
+        self.code_expiry = timezone.now() + timedelta(minutes=3)
+        self.save()
+        return code
+    
+    objects = CustomUserManager()
